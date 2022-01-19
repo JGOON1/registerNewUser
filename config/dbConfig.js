2c78dd7d62f9ecadbf3404827f7fa0d723675442
unknown <john.meng014@gmail.com>
2022-01-19 11:29:51 -0500

    added db config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,17 @@
 module.exports = {
-    HOST: 'database-hash.ccq70wy37xi3.us-east-1.rds.amazonaws.com', // your endpoint
-    USER: 'postgres_johnm', // your username
-    PASSWORD: '1q2w3e4r', // your password
-    DB: 'sql_hashtest_db', // your db name
+    HOST: process.env.HOST, // your endpoint
+    USER: process.env.USER, // your username
+    PASSWORD: process.env.PASSWORD, // your password
+    DB: process.env.DB, // your db name
     dialect: 'postgres',
  }
+
+
+
+// module.exports = {
+//     HOST: 'database-hash.ccq70wy37xi3.us-east-1.rds.amazonaws.com', // your endpoint
+//     USER: 'postgres_johnm', // your username
+//     PASSWORD: '1q2w3e4r', // your password
+//     DB: 'sql_hashtest_db', // your db name
+//     dialect: 'postgres',
+//  }
--- models/index.js
+++ models/index.js
@@ -1,15 +1,29 @@
 const dbConfig = require('../config/dbConfig');
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(
-    { // use imported configurations from dbConfig
-        database: dbConfig.DB,
-        username: dbConfig.USER,
-        password: dbConfig.PASSWORD,
-        dialect: dbConfig.dialect,
-        host: dbConfig.HOST,
+
+let sequelize = null;
+
+if (process && process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
-)
+    );
+} else {
+    sequelize = new Sequelize(
+        { // use imported configurations from dbConfig
+            database: dbConfig.DB,
+            username: dbConfig.USER,
+            password: dbConfig.PASSWORD,
+            dialect: dbConfig.dialect,
+            host: dbConfig.HOST,
+        })
+}
 
 // authenticate will test the connection with DB and return a promise
 sequelize.authenticate()
@@ -27,11 +41,11 @@ const db = {}
 //create an attribute storing the prevously sequlize instance
 db.sequelize = sequelize;
 //get the customer model
-db.newUsers = require('./registerModel') (sequelize, DataTypes);
+db.newUsers = require('./registerModel')(sequelize, DataTypes);
 // sync the db by running the model
-db.sequelize.sync({force: false}).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
     console.log('DB synced with sequelize')
-}).catch((error) =>{
+}).catch((error) => {
     console.log('Error syncing the DB to sequelize ' + error);
 });
 // lastly export the db
