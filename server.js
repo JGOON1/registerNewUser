a9eb1d64a0c1cc58aed0482a70be49551829e558
unknown <john.meng014@gmail.com>
2022-01-19 11:18:25 -0500

    process env port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routers = require('./routes/signupRouter.js')
 const app = express();
-const port = 3050
+const port = process.env.PORT || 3050;
 
 
 app.use(express.json())
